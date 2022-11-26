8c69edcf624cf467fda11eddea2f0957f10481a3
Idhibhat Pankam <marginpankam@gmail.com>
2022-11-26 23:08:23 +0700

    navbar

--- apps/client/src/core/components/Navbar.tsx
+++ apps/client/src/core/components/Navbar.tsx
@@ -1,5 +1,15 @@
 import type { FC } from "react";
 
 export const Navbar: FC = () => {
-  return <div>aa</div>;
+  return (
+    <div className="w-screen h-14 drop-shadow bg-white flex px-12 items-center text-lg font-semibold">
+      <span className="text-blue-500">Th</span>
+      <span className="text-red-500">ai</span>
+      <span className="text-amber-500">2</span>
+      <span className="text-blue-500">K</span>
+      <span className="text-green-600">a</span>
+      <span className="text-red-500 mr-2">na</span>
+      <span className="text-gray-500">Translate</span>
+    </div>
+  );
 };
