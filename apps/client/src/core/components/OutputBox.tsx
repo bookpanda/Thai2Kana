ec4b0bb882fca690785c7cf0d1283a2635c1f1ef
Idhibhat Pankam <marginpankam@gmail.com>
2022-11-30 23:11:22 +0700

    outputbox conditional rendering

--- apps/client/src/core/components/OutputBox.tsx
+++ apps/client/src/core/components/OutputBox.tsx
@@ -6,7 +6,14 @@ export const OutputBox: FC = () => {
   const context = useAppContext();
   const { katakana } = context;
   return (
-    <div className="w-screen md:w-1/2 min-h-36 p-4 outline-0 border-b border-gray-300 drop-shadow-md bg-blue-600 md:bg-gray-100 text-2xl text-white md:text-black md:border-l">
+    <div
+      className={`w-screen md:w-1/2 min-h-36 p-4 outline-0 border-b border-gray-300 drop-shadow-md bg-blue-600 
+    ${
+      katakana
+        ? "md:bg-gray-100 md:text-black"
+        : "hidden md:block md:bg-white md:text-neutral-600"
+    } text-2xl text-white md:border-l`}
+    >
       <p style={{ wordBreak: "break-word" }} className="w-100%">
         {katakana || "Translation"}
       </p>
