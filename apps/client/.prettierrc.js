9071a7095425fbff5d50e8facccf66ee4b60b063
Idhibhat Pankam <marginpankam@gmail.com>
2022-12-03 00:02:15 +0700

    removed ui package

--- apps/client/.prettierrc.js
+++ apps/client/.prettierrc.js
@@ -1,2 +1,2 @@
-// module.exports = require("@thaitokana/config/prettier-app");
-module.exports = require("../../packages/config/prettier-app");
+module.exports = require("@thaitokana/config/prettier-app");
+// module.exports = require("../../packages/config/prettier-app");
